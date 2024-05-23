59c4e1c5dc24fa1151a76fc93237eb22bc53bb7f
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-23 15:58:09 +0530

    api file changes

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -2,7 +2,7 @@ const handler = (req: any, res: any) => {
   if (req.method === "GET") {
     const redirectUrl =
       process.env.RedirectURL || "http://localhost:3000/destination";
-    res.status(308).setHeader("Location", redirectUrl);
+    res.writeHead(308, { Location: redirectUrl });
     res.end();
   } else {
     res.setHeader("Allow", ["GET"]);
